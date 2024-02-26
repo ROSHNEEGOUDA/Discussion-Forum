25b1790204b11135711b19948da1b10a258ccbee
ROSHNEE_GOUDA <120122742+ROSHNEEGOUDA@users.noreply.github.com>
2024-02-26 20:14:29 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -2,5 +2,5 @@ export const api = {
   tagsEndPoint: 'https://discussion-forum-server-flame.vercel.app/tags/',
   usersEndPoint: 'https://discussion-forum-server-flame.vercel.app/users/',
   postsEndPoint: 'https://discussion-forum-server-flame.vercel.app/posts/',
-  repliesEndPoint: 'https://discussion-forum-server-flame.vercel.app/reply',
+  repliesEndPoint: 'https://discussion-forum-server-flame.vercel.app/reply/',
 }
