f433c55f3bdbfefe0dca6482fbac2e07343e8f15
ROSHNEE_GOUDA <120122742+ROSHNEEGOUDA@users.noreply.github.com>
2024-02-26 20:14:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 app.get("/", (req, res) => {
   res.send("request successfully sent!");
