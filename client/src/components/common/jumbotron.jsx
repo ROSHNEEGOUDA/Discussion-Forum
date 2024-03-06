b350a0229a57c256742a63ef53d3586e99d1cc62
ROSHNEE_GOUDA <120122742+ROSHNEEGOUDA@users.noreply.github.com>
2024-03-06 13:44:26 +0530

    Update jumbotron.jsx

--- client/src/components/common/jumbotron.jsx
+++ client/src/components/common/jumbotron.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Jumotron = () => {
   return (
     <div className="jumbotron">
-      <h1 className="display-3">Community</h1>
+      <h1 className="display-3">Welcom to CodePeace Community</h1>
       <p class="lead">We &lt;3 people who code!</p>
       <hr class="my-4" />
-      <p className="lead">A disscussion forum for enthusiastic developers.</p>
+      <p className="lead">A disscussion forum for enthusiastic developers. Where you can post your Question, Quaries and Suggestions.</p>
     </div>
   );
 };
